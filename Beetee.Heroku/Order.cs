afdde076387a425cc0d06bea15584f643dce99f2
avagyansamvel <avagyansamvel07@gmail.com>
2022-08-03 13:41:08 +0400

    Add Order model

---
+++ Beetee.Heroku/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace Beetee.Heroku
+{
+    public enum WebsiteType
+    {
+        LandingPage,
+        OnlineShop,
+        Blog,
+        News
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public WebsiteType WebsiteType { get; set; }
+    }
+}
